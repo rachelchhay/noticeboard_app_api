0aee404b44e1a4916f7159ea27d0c98f4f29b525
Rachel Chhay <rachelchhay@Rachels-MacBook-Pro.local>
2017-09-05 15:07:56 -0500

    changed cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:2000'
+    origins 'example.com'
 
     resource '*',
       headers: :any,
-      methods: [:get, :options, :head]
+      methods: [:get, :post, :update, :delete, :options, :head]
   end
 end
